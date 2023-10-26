e24e82cfe0d1c70c6f9941945eba1e5876297b88
Herdiyana Firmansyah <96756648+Din-27@users.noreply.github.com>
2023-10-26 14:33:12 +0700

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -9,43 +9,54 @@ const io = require("socket.io")(server, {
 
 const PORT = 4000;
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
-
-
-let senderData = {};
+const API = axios.create({
+  baseURL: 'http://localhost:3004/'
+})
 
 io.on("connection", (socket) => {
   const { roomId, identity } = socket.handshake.query;
   console.log(`Client ${identity} connected`);
-
+  API.get('/client_aktif')
+    .then(({ data }) => {
+      if (data.filter(x => x.identity === identity).length === 0)
+        API.post('/client_aktif', { identity: identity }).then()
+    })
   // Join a conversation
   socket.join(roomId);
 
-  // Listen for new messages
-  socket.on(NEW_CHAT_MESSAGE_EVENT, async (data) => {
-
-    if (!identity.includes('chatbot')) {
-      if (data.body.match(/[o|O][R|r][d|D][e|E][r|R]/gm)) {
-        setTimeout(() => {
-          axios.post('http://localhost:3004/pesan', { identity: 'chatbot', body: 'pesan apa kak?' })
-            .then((res) => {
-              io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, { identity: 'chatbot', body: 'pesan apa kak?' });
-            })
-            .catch((e) => console.log(e))
-        }, 2000)
-      }
-    }
-    axios.post('http://localhost:3004/pesan', data)
-      .then((res) => {
-        io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
-      })
-      .catch((e) => console.log(e))
-  });
-
-  // Leave the room if the user closes the socket
   socket.on("disconnect", () => {
     console.log(`Client ${identity} diconnected`);
+    API.get('/client_aktif')
+      .then(({ data }) => {
+        const getId = data.filter(x => x.identity === identity)[0]
+        API.delete(`/client_aktif/${getId.id}`)
+      }).catch(e => console.log(e))
     socket.leave(roomId);
   });
+
+  socket.on(NEW_CHAT_MESSAGE_EVENT, async (data) => {
+    API.get('/client_aktif')
+      .then((res) => {
+        // console.log(res.data.filter(x => x.identity === 'chatbot'),
+        //   !identity.includes('chatbot') && res.data.filter(x => x.identity === 'chatbot').length > 0);
+        if (!identity.includes('chatbot') && res.data.filter(x => x.identity === 'chatbot').length > 0) {
+          if (data.body.match(/[o|O][R|r][d|D][e|E][r|R]/gm)) {
+            setTimeout(() => {
+              API.post('/pesan', { identity: 'chatbot', body: 'pesan apa kak?' })
+                .then(() => {
+                  io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, { identity: 'chatbot', body: 'pesan apa kak?' });
+                })
+                .catch((e) => console.log(e))
+            }, 2000)
+          }
+        }
+        API.post('/pesan', data)
+          .then(() => {
+            io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+          })
+          .catch((e) => console.log(e))
+      });
+  })
 });
 
 server.listen(PORT, () => {
